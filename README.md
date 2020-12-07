6c9c6c0527e5fb6223aa9f3dbd11677936f52a5f
vbstreetz <vbstreetz@gmail.com>
2020-12-07 20:20:39 +0300

    docs: init getting started guide

--- README.md
+++ README.md
@@ -2,7 +2,22 @@
 
 ### ZKPay
 
-Create simple payment, donation etc links.
-Users experience fast + cheap sends, powered by [zkSync](zksync.io).
+Quickest way to create page links to accept payments, donation etc.
+Users have the option to pay cheaply and fast with [zkSync](https://zksync.io) or regular network.
 
 Project link: https://zkpay.link
+
+### Getting started
+
+#### Prerequisites
+
+- Node.JS 12+
+- Git
+
+#### Running the app
+
+Todo.
+
+### How it works
+
+Todo.
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,32 +1,32 @@
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const rateLimit = require("express-rate-limit")
-const morgan = require("morgan")
-const {PRODUCTION, TEST} = require("config")
-const auth = require("./auth")
-const links = require("./links")
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const rateLimit = require('express-rate-limit')
+const morgan = require('morgan')
+const {PRODUCTION, TEST} = require('config')
+const auth = require('./auth')
+const links = require('./links')
 
 const apiLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   max: 30,
   message:
-    "Too many requests created from this IP, please try again in 5 minutes 😜",
+    'Too many requests created from this IP, please try again in 5 minutes 😜',
 })
 
 const app = (module.exports = express())
 if (!TEST) {
-  app.set("trust proxy", 1)
-  app.use(morgan("tiny"))
+  app.set('trust proxy', 1)
+  app.use(morgan('tiny'))
   app.use(cors())
 }
 app.use(bodyParser.json())
-app.use("/auth", apiLimiter, auth)
-app.use("/links", apiLimiter, links)
+app.use('/auth', apiLimiter, auth)
+app.use('/links', apiLimiter, links)
 app.use(function (err, req, res, next) {
   err.status = err.status || 500
   if (!TEST) {
     console.error(err.stack)
   }
-  res.status(err.status).json(PRODUCTION ? "something broke!" : err.message)
+  res.status(err.status).json(PRODUCTION ? 'something broke!' : err.message)
 })
